1e345a9885a0933ccd9d502b557887f011fea353
antoniosantro <42532152+antoniosantro@users.noreply.github.com>
2018-09-13 22:45:34 +0300

    Add files via upload

---
+++ lessen 6/header/js/main.js
@@ -0,0 +1,7 @@
+$('.button-one').click(function(){
+  $('.image-fonst').addClass('paragraph2'); // Замена класса
+});
+
+$('.button-two').click(function(){
+  $('.image-fonst').removeClass('paragraph2'); // удаление класса
+});
